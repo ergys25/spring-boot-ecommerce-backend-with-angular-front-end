9761664bc05eebc586712f5330f4cdef575843e4
Ergys Hila <ergysmania@gmail.com>
2024-02-15 15:57:45 -0500

    "feat: Update Product and ProductCategory entities in `codes.ergyshila.springbootecommerce.entiy` package
    The Product and ProductCategory entities have been updated with new fields and annotations. The changes include:
    
    For Product:
    - Added `@ManyToOne` relationship with `ProductCategory` entity.
    - Added new fields: `sku`, `name`, `description`, `unitPrice`, `imageUrl`, `active`, `unitsInStock`, `dateCreated`, `lastUpdated`.
    - Implemented `equals` and `hashCode` methods to handle Hibernate proxy objects.
    
    For ProductCategory:
    - Added `@OneToMany` relationship with `Product` entity.
    - Added new field: `categoryName`.
    - Implemented `equals` and `hashCode` methods to handle Hibernate proxy objects.
    
    These changes are part of the ongoing work to enhance the e-commerce application's data model."

---
+++ src/main/java/codes/ergyshila/springbootecommerce/entiy/ProductCategory.java
@@ -0,0 +1,45 @@
+package codes.ergyshila.springbootecommerce.entiy;
+
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.proxy.HibernateProxy;
+
+import java.util.Objects;
+import java.util.Set;
+
+@Entity
+@Getter
+@Setter
+@ToString
+@RequiredArgsConstructor
+@Table(name = "product_category")
+public class ProductCategory {
+
+    @jakarta.persistence.Id
+    @GeneratedValue(strategy = jakarta.persistence.GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long Id;
+
+    @Column(name = "category_name")
+    private String categoryName;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "category")
+    @ToString.Exclude
+    private Set<Product> products;
+
+    @Override
+    public final boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null) return false;
+        Class<?> oEffectiveClass = o instanceof HibernateProxy ? ((HibernateProxy) o).getHibernateLazyInitializer().getPersistentClass() : o.getClass();
+        Class<?> thisEffectiveClass = this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass() : this.getClass();
+        if (thisEffectiveClass != oEffectiveClass) return false;
+        ProductCategory that = (ProductCategory) o;
+        return getId() != null && Objects.equals(getId(), that.getId());
+    }
+
+    @Override
+    public final int hashCode() {
+        return this instanceof HibernateProxy ? ((HibernateProxy) this).getHibernateLazyInitializer().getPersistentClass().hashCode() : getClass().hashCode();
+    }
+}
