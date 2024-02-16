0e92bc63779def375cc550dd78f4c397add5cc7f
Ergys Hila <ergysmania@gmail.com>
2024-02-16 08:35:20 -0500

    Add CrossOrigin

--- src/main/java/codes/ergyshila/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/codes/ergyshila/springbootecommerce/dao/ProductRepository.java
@@ -2,6 +2,8 @@ package codes.ergyshila.springbootecommerce.dao;
 
 import codes.ergyshila.springbootecommerce.entiy.Product;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+@CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
 }
