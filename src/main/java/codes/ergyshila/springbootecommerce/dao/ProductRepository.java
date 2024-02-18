cd69f07838a8f67c7e902fef1946f180dac1458d
Ergys Hila <ergysmania@gmail.com>
2024-02-17 20:49:17 -0500

    Add routing

--- src/main/java/codes/ergyshila/springbootecommerce/dao/ProductRepository.java
+++ src/main/java/codes/ergyshila/springbootecommerce/dao/ProductRepository.java
@@ -1,9 +1,12 @@
 package codes.ergyshila.springbootecommerce.dao;
 
 import codes.ergyshila.springbootecommerce.entiy.Product;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.web.bind.annotation.CrossOrigin;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.data.domain.Page;
 @CrossOrigin("http://localhost:4200")
 public interface ProductRepository extends JpaRepository<Product, Long> {
+    Page<Product> findByCategoryId(@RequestParam("id") Long id, Pageable pageable);
 }
